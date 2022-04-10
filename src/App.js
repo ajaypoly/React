af69155730c54cdc0ac148a19cc0f8508c760f9b
AJAY POLY <ajay@hamon.in>
2022-04-10 14:32:30 +0530

    add increment,decrement and reset function

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ import {useState} from 'react'
 
 function App() {
   const [counter,setCounter]=useState(0)
+
+  const [data, setData]=useState(10)
   function increment(){
     setCounter(counter+1)
+  }
+  function decrement(){
+    setData(data-1)
+  }
+  function reset(){
+    setCounter(1)
+    setData(10)
   }
   
   return (
@@ -16,8 +25,10 @@ function App() {
 
   <div>
      <h1>{counter}</h1>
-
+     <h2>{data}</h2>
+     <button onClick={reset}>Reset</button>
      <button onClick={increment}>Click mee</button>
+     <button onClick={decrement}> don,t</button>
   </div> 
     
   );
