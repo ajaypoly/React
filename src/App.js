96f99d9bf9ea5b06f6f4bf9511522cb8be136416
AJAY POLY <ajay@hamon.in>
2022-04-10 15:16:16 +0530

    map implentaION example

--- src/App.js
+++ src/App.js
@@ -6,12 +6,28 @@ import React from 'react'
 
 function App() {
 
-  const [name ,setName]=useState('abc')
-  const [value,setValue]=useState('efgf')
+  const [name ,setName]=useState()
+  const [value,setValue]=useState()
+  const data =[
+    {name:'ajay', age:22},
+    {name:'akhil', age:22},
+    {name:'anil', age:24},
+    {name:'disan', age:25},
+    {name:'jibin', age:27},
+    {name:'sarath', age:27},
+    {name:'joffin', age:29},
+  ]
+
+
   return (
     <div>
-      <Random data={name}
-      item={value}/>
+      {data.map((item)=>(
+        <>
+        <h1>{item.name}</h1>
+        <span>age:{item.age}</span>
+        </>
+      )
+      )}
     </div>
   )
 }
