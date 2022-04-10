58098e2d7df9de401ec70632ffe53a4d1353366c
AJAY POLY <ajay@hamon.in>
2022-04-10 17:31:45 +0530

    add navbar using bootwatch and add background color to all components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import "./App.css";
-import Random from "./Random";
-import New from "./New";
-import { useState } from "react";
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+
 
 function App() {
-
-  const [name ,setName]=useState()
-  const [value,setValue]=useState()
-  const data =[
-    {name:'ajay', age:22},
-    {name:'akhil', age:22},
-    {name:'anil', age:24},
-    {name:'disan', age:25},
-    {name:'jibin', age:27},
-    {name:'sarath', age:27},
-    {name:'joffin', age:29},
-  ]
-
-
   return (
     <div>
-      {data.map((item)=>(
-        <>
-        <h1>{item.name}</h1>
-        <span>age:{item.age}</span>
-        </>
-      )
-      )}
+      <Header/>
     </div>
   )
 }
